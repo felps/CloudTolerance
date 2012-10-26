c86631eb9b2a5eef057034b362c51a883de15dde
felps <fpontes100@hotmail.com>
2012-10-26 11:11:05 -0200

    Generic proxy first drafts for dynamic invokation. Still untested.

--- CloudTolerance/src/main/proxy/GenericProxy.java
+++ CloudTolerance/src/main/proxy/GenericProxy.java
@@ -1,10 +1,19 @@
 package proxy;
 
+import javax.xml.ws.FaultAction;
+
+import techniques.FaultToleranceTechnique;
+
 public class GenericProxy {
 
-	public String techniqueName;
+	public FaultToleranceTechnique technique;
 	public String configFile;
 	public String wsGroupName;
+	
+	private void updateTechnique(FaultToleranceTechnique newTechnique) {
+		
+
+	}
 	
 	
 	
