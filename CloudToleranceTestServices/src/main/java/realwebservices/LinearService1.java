f68449d9cbc8f0ff0681042b88f7ed6d9fe73bc6
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-04 16:34:36 -0200

    Linear Service: A WS that adds one to an integer

---
+++ CloudToleranceTestServices/src/main/java/realwebservices/LinearService1.java
@@ -0,0 +1,44 @@
+package realwebservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService
+public class LinearService1 extends FaultyWebService {
+
+	public LinearService1(double failStop, double faultyResponse) {
+		this.setFailstopProbability(failStop);
+		this.setFaultyResponseProbability(faultyResponse);
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 3){
+			System.out
+					.println("USAGE: <WSname> <WS fail-stop probability> <WS faulty response probability> <endpoint>");
+			System.exit(0);
+		}
+
+		double failStop = Double.parseDouble(args[0]);
+		double faultyResponse = Double.parseDouble(args[1]);
+
+		LinearService1 ws = new LinearService1(failStop, faultyResponse);
+
+		Endpoint.publish(args[2], ws);
+
+		System.out.println("Web service up and Running! \n"
+				+ "Fail-stop probability:       " + failStop + "\n"
+				+ "Faulty Response probability: " + faultyResponse);
+	}
+
+	@WebMethod
+	public int addOne(int number) {
+		emulateFailStopFailures();
+		if (willReturnWrongAnswer()) {
+			return getRandomIntegerAnswer();
+		}
+
+		return number + 1;
+
+	}
+}
---
+++ CloudToleranceTestServices/src/test/java/realwebservices/LinearService1TestMainRaiseService.java
@@ -0,0 +1,33 @@
+package realwebservices;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sun.xml.internal.ws.api.EndpointAddress;
+
+public class LinearService1TestMainRaiseService {
+
+	@BeforeClass
+	public static void BeforeClass() throws InterruptedException {
+		String[] args = new String[3];
+		args[0] = 0.0 + "";
+		args[1] = 0.0 + "";
+		args[2] = "http://0.0.0.0:2304/linear";
+		
+		LinearService1.main(args);
+//		Thread.sleep(20000);
+	}
+
+	@Test
+	public void checkServiceAvailability() throws URISyntaxException, IOException {
+		EndpointAddress ep = new EndpointAddress(
+				"http://localhost:2304/linear?wsdl");
+		ep.openConnection().getContent();
+		System.out.println("Service is running already");
+	}
+
+	
+}
