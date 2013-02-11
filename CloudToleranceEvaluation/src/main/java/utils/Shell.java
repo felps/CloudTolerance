92fab105567e1c57e90c9cb1ea342237fd4af107
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-11 17:08:28 -0200

    Added shell utility for running shell commands

---
+++ CloudToleranceEvaluation/src/main/java/utils/Shell.java
@@ -0,0 +1,53 @@
+package utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.Process;
+
+public class Shell {
+
+	public void runCommandNow(String command) {
+		int pid = -1;
+
+		try {
+			// Run ls command
+			Process process = Runtime.getRuntime().exec(command);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
+	public String runCommandAndGetOutput(String command) {
+
+		String output = "";
+		try {
+			Process proc = Runtime.getRuntime().exec(command);
+			int result = proc.waitFor();
+			BufferedInputStream buffer = new BufferedInputStream(
+					proc.getInputStream());
+
+			BufferedReader commandOutput = new BufferedReader(
+					new InputStreamReader(buffer));
+
+			String line = null;
+
+			while ((line = commandOutput.readLine()) != null) {
+				output.concat(line + '\n');
+			}
+
+			commandOutput.close();
+
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return output;
+	}
+
+}
