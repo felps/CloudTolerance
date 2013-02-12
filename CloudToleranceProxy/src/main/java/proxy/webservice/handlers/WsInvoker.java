409248f45f45e1e9bd7d8b139155fd21c5659dd2
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-12 18:18:31 -0200

    Added enhanced timeout functionality.
    Previous version would always timeout at 60 seconds

--- CloudToleranceProxy/src/main/java/proxy/webservice/handlers/WsInvoker.java
+++ CloudToleranceProxy/src/main/java/proxy/webservice/handlers/WsInvoker.java
@@ -1,10 +1,9 @@
 package proxy.webservice.handlers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 
 public class WsInvoker {
 
@@ -18,26 +17,24 @@ public class WsInvoker {
 	}
 
 	public void setTimeout(long newTimeout) {
-		this.timeout = newTimeout;
+		
+		timeout=newTimeout;
+		HTTPConduit http = (HTTPConduit) webServiceClient.getConduit();
+        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+//        ClassLoader clAfterClientPolicy = httpClientPolicy.getClass().getClassLoader();
+        httpClientPolicy.setConnectionTimeout(newTimeout);
+        http.setClient(httpClientPolicy);
 	}
 
 	public WsInvoker(String wsdlURL) {
 		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
 		webServiceClient = dcf.createClient(wsdlURL);
 		this.wsdlURL = wsdlURL;
-//		pendingRequests = new ArrayList<WsInvokation>();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public WsInvokation invokeWebMethod(String methodName, Object... args) {
 		WsInvokation invokation = new WsInvokation(wsdlURL, webServiceClient,
 				timeout, methodName, args);
-//		pendingRequests.add(invokation);
 		invokation.perform();
 		return invokation;
 	}
