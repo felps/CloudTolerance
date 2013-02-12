f9996b5d9766e1d1be404440f1484394ae2fa7de
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-12 18:57:15 -0200

    Scripts for starting the choreography's proxies

---
+++ CloudToleranceEvaluation/MultipleMessagesChoreographyProxies.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2202/proxy http://127.0.0.1:2102/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2203/proxy http://127.0.0.1:2103/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2204/proxy http://127.0.0.1:2104/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2205/proxy http://127.0.0.1:2105/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2206/proxy http://127.0.0.1:2106/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2207/proxy http://127.0.0.1:2107/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2208/proxy http://127.0.0.1:2108/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2209/proxy http://127.0.0.1:2109/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
+java -jar ProxyEndpoint.jar http://0.0.0.0:2210/proxy http://127.0.0.1:2110/choreography?wsdl addOne http://godzilla.ime.usp.br:2302/Linear?wsdl &
+
