c262cd9fdfa773d8b9a265a07dd5cc6267759977
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-19 14:58:15 -0300

    Parallelized script

--- CloudToleranceEvaluation/Scripts/copyFiles.sh
+++ CloudToleranceEvaluation/Scripts/copyFiles.sh
@@ -2,14 +2,14 @@
 
 source locations.sh
 
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server1:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server2:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server3:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server4:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server5:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server6:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server7:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server8:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server9:
-scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server0:
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server1: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server2: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server3: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server4: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server5: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server6: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server7: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server8: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server9: &
+scp ProxyEndpoint.jar ChoreographyEndpointService.jar MultipleMessagesChoreographyEndpointService.jar $server0: &
 
