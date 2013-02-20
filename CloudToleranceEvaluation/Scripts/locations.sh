fdad34a6632cbedbe383abebf1d941f4a51b4225
Felipe Guimarães <felipepg@ime.usp.br>
2013-02-20 11:45:18 -0300

    new addresses

--- CloudToleranceEvaluation/Scripts/locations.sh
+++ CloudToleranceEvaluation/Scripts/locations.sh
@@ -2,13 +2,13 @@
 
 echo "Setting server addresses..."
 
-server1=ubuntu@54.232.16.228
-server2=ubuntu@54.232.16.240
-server3=ubuntu@177.71.156.60
-server4=ubuntu@54.232.36.215
-server5=ubuntu@54.232.20.250
-server6=ubuntu@177.71.155.214
-server7=ubuntu@177.71.152.81
-server8=ubuntu@54.232.29.39
-server9=ubuntu@177.71.152.79
-server0=ubuntu@54.232.29.236
+server1=177.71.231.104
+server2=54.232.39.147
+server3=177.71.156.49
+server4=177.71.154.157
+server5=177.71.130.1
+server6=54.232.15.99
+server7=177.71.152.204
+server8=54.232.17.3
+server9=177.71.230.246
+server0=177.71.236.5
