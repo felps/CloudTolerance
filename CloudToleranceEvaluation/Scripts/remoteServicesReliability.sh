2c06326ee12632662076ae0215bcf98256775cd8
Felipe Guimarães <felipepg@ime.usp.br>
2013-03-06 14:58:33 -0300

    Added unreliable response services

--- CloudToleranceEvaluation/Scripts/remoteServicesReliability.sh
+++ CloudToleranceEvaluation/Scripts/remoteServicesReliability.sh
@@ -20,3 +20,21 @@ do
 
 	sleep 5
 done
+
+basePort=23000
+
+for ((incorrectResponseProbability=0; incorrectResponseProbability<=10;incorrectResponseProbability++))
+do
+
+	port=$((basePort + 100 -incorrectResponseProbability))
+
+	echo
+	echo "ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$port/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &"
+	ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$port/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &
+	echo "ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$((port+1000))/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &"
+	ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$((port+1000))/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &
+	echo "ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$((port+2000))/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &"
+	ssh -p $serviceProviderPort $serviceProviderUser@$serviceProvider ./restartServiceAutomatically.sh http://0.0.0.0:$((port+2000))/Linear 0.0 0.0$incorrectResponseProbability 0 LinearService.jar &
+
+	sleep 5
+done
