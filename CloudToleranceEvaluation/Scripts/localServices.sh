4ea5c92a81e007dc9ba0174f73d6d34db802cc94
Felipe Guimarães <felipepg@ime.usp.br>
2013-03-06 17:41:09 -0300

    Minor modifications

---
+++ CloudToleranceEvaluation/Scripts/localServices.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+
+java -jar LinearService.jar 0.10 0.0 http://0.0.0.0:22090/Linear &
+java -jar LinearService.jar 0.09 0.0 http://0.0.0.0:22091/Linear &
+java -jar LinearService.jar 0.08 0.0 http://0.0.0.0:22092/Linear &
+java -jar LinearService.jar 0.07 0.0 http://0.0.0.0:22093/Linear &
+java -jar LinearService.jar 0.06 0.0 http://0.0.0.0:22094/Linear &
+java -jar LinearService.jar 0.05 0.0 http://0.0.0.0:22095/Linear &
+java -jar LinearService.jar 0.04 0.0 http://0.0.0.0:22096/Linear &
+java -jar LinearService.jar 0.03 0.0 http://0.0.0.0:22097/Linear &
+java -jar LinearService.jar 0.02 0.0 http://0.0.0.0:22098/Linear &
+java -jar LinearService.jar 0.01 0.0 http://0.0.0.0:22099/Linear &
+java -jar LinearService.jar 0.00 0.0 http://0.0.0.0:22100/Linear &
+
+java -jar LinearService.jar 0.10 0.0 http://0.0.0.0:23090/Linear &
+java -jar LinearService.jar 0.09 0.0 http://0.0.0.0:23091/Linear &
+java -jar LinearService.jar 0.08 0.0 http://0.0.0.0:23092/Linear &
+java -jar LinearService.jar 0.07 0.0 http://0.0.0.0:23093/Linear &
+java -jar LinearService.jar 0.06 0.0 http://0.0.0.0:24094/Linear &
+java -jar LinearService.jar 0.05 0.0 http://0.0.0.0:23095/Linear &
+java -jar LinearService.jar 0.04 0.0 http://0.0.0.0:23096/Linear &
+java -jar LinearService.jar 0.03 0.0 http://0.0.0.0:23097/Linear &
+java -jar LinearService.jar 0.02 0.0 http://0.0.0.0:23098/Linear &
+java -jar LinearService.jar 0.01 0.0 http://0.0.0.0:23099/Linear &
+java -jar LinearService.jar 0.01 0.0 http://0.0.0.0:23100/Linear &
+
+
+java -jar LinearService.jar 0.10 0.0 http://0.0.0.0:24090/Linear &
+java -jar LinearService.jar 0.09 0.0 http://0.0.0.0:24091/Linear &
+java -jar LinearService.jar 0.08 0.0 http://0.0.0.0:24092/Linear &
+java -jar LinearService.jar 0.07 0.0 http://0.0.0.0:24093/Linear &
+java -jar LinearService.jar 0.06 0.0 http://0.0.0.0:24094/Linear &
+java -jar LinearService.jar 0.05 0.0 http://0.0.0.0:24095/Linear &
+java -jar LinearService.jar 0.04 0.0 http://0.0.0.0:24096/Linear &
+java -jar LinearService.jar 0.03 0.0 http://0.0.0.0:24097/Linear &
+java -jar LinearService.jar 0.02 0.0 http://0.0.0.0:24098/Linear &
+java -jar LinearService.jar 0.01 0.0 http://0.0.0.0:24099/Linear &
+java -jar LinearService.jar 0.00 0.0 http://0.0.0.0:24100/Linear &
+
+java -jar LinearService.jar 0.0 0.10 http://0.0.0.0:25090/Linear &
+java -jar LinearService.jar 0.0 0.09 http://0.0.0.0:25091/Linear &
+java -jar LinearService.jar 0.0 0.08 http://0.0.0.0:25092/Linear &
+java -jar LinearService.jar 0.0 0.07 http://0.0.0.0:25093/Linear &
+java -jar LinearService.jar 0.0 0.06 http://0.0.0.0:25094/Linear &
+java -jar LinearService.jar 0.0 0.05 http://0.0.0.0:25095/Linear &
+java -jar LinearService.jar 0.0 0.04 http://0.0.0.0:25096/Linear &
+java -jar LinearService.jar 0.0 0.03 http://0.0.0.0:25097/Linear &
+java -jar LinearService.jar 0.0 0.02 http://0.0.0.0:25098/Linear &
+java -jar LinearService.jar 0.0 0.01 http://0.0.0.0:25099/Linear &
+java -jar LinearService.jar 0.0 0.00 http://0.0.0.0:25100/Linear &
+
+java -jar LinearService.jar 0.0 0.10 http://0.0.0.0:26090/Linear &
+java -jar LinearService.jar 0.0 0.09 http://0.0.0.0:26091/Linear &
+java -jar LinearService.jar 0.0 0.08 http://0.0.0.0:26092/Linear &
+java -jar LinearService.jar 0.0 0.07 http://0.0.0.0:26093/Linear &
+java -jar LinearService.jar 0.0 0.06 http://0.0.0.0:26094/Linear &
+java -jar LinearService.jar 0.0 0.05 http://0.0.0.0:26095/Linear &
+java -jar LinearService.jar 0.0 0.04 http://0.0.0.0:26096/Linear &
+java -jar LinearService.jar 0.0 0.03 http://0.0.0.0:26097/Linear &
+java -jar LinearService.jar 0.0 0.02 http://0.0.0.0:26098/Linear &
+java -jar LinearService.jar 0.0 0.01 http://0.0.0.0:26099/Linear &
+java -jar LinearService.jar 0.0 0.00 http://0.0.0.0:26100/Linear &
+
+java -jar LinearService.jar 0.0 0.10 http://0.0.0.0:27090/Linear &
+java -jar LinearService.jar 0.0 0.09 http://0.0.0.0:27091/Linear &
+java -jar LinearService.jar 0.0 0.08 http://0.0.0.0:27092/Linear &
+java -jar LinearService.jar 0.0 0.07 http://0.0.0.0:27093/Linear &
+java -jar LinearService.jar 0.0 0.06 http://0.0.0.0:27094/Linear &
+java -jar LinearService.jar 0.0 0.05 http://0.0.0.0:27095/Linear &
+java -jar LinearService.jar 0.0 0.04 http://0.0.0.0:27096/Linear &
+java -jar LinearService.jar 0.0 0.03 http://0.0.0.0:27097/Linear &
+java -jar LinearService.jar 0.0 0.02 http://0.0.0.0:27098/Linear &
+java -jar LinearService.jar 0.0 0.01 http://0.0.0.0:27099/Linear &
+java -jar LinearService.jar 0.0 0.00 http://0.0.0.0:27100/Linear &
+
+
--- CloudToleranceEvaluation/src/test/java/evaluation/ReliabilityEvaluation.java
+++ CloudToleranceEvaluation/src/test/java/evaluation/ReliabilityEvaluation.java
@@ -13,29 +13,54 @@ import proxy.webservice.handlers.WsInvoker;
 
 public class ReliabilityEvaluation {
 
-	private static final String WS_100_RELIABILITY_NOFT = "http://127.0.0.1:21100/choreography?wsdl";
-	private static final String WS_99_RELIABILITY_NOFT = "http://127.0.0.1:21099/choreography?wsdl";
-	private static final String WS_98_RELIABILITY_NOFT = "http://127.0.0.1:21098/choreography?wsdl";
-	private static final String WS_97_RELIABILITY_NOFT = "http://127.0.0.1:21097/choreography?wsdl";
-	private static final String WS_96_RELIABILITY_NOFT = "http://127.0.0.1:21096/choreography?wsdl";
-	private static final String WS_95_RELIABILITY_NOFT = "http://127.0.0.1:21095/choreography?wsdl";
-	private static final String WS_94_RELIABILITY_NOFT = "http://127.0.0.1:21094/choreography?wsdl";
-	private static final String WS_93_RELIABILITY_NOFT = "http://127.0.0.1:21093/choreography?wsdl";
-	private static final String WS_92_RELIABILITY_NOFT = "http://127.0.0.1:21092/choreography?wsdl";
-	private static final String WS_91_RELIABILITY_NOFT = "http://127.0.0.1:21091/choreography?wsdl";
-	private static final String WS_90_RELIABILITY_NOFT = "http://127.0.0.1:21090/choreography?wsdl";
+	private static final String WS_100_FAILSTOP_NO_FT = "http://127.0.0.1:21100/choreography?wsdl";
+	private static final String WS_99_FAILSTOP_NO_FT = "http://127.0.0.1:21099/choreography?wsdl";
+	private static final String WS_98_FAILSTOP_NO_FT = "http://127.0.0.1:21098/choreography?wsdl";
+	private static final String WS_97_FAILSTOP_NO_FT = "http://127.0.0.1:21097/choreography?wsdl";
+	private static final String WS_96_FAILSTOP_NO_FT = "http://127.0.0.1:21096/choreography?wsdl";
+	private static final String WS_95_FAILSTOP_NO_FT = "http://127.0.0.1:21095/choreography?wsdl";
+	private static final String WS_94_FAILSTOP_NO_FT = "http://127.0.0.1:21094/choreography?wsdl";
+	private static final String WS_93_FAILSTOP_NO_FT = "http://127.0.0.1:21093/choreography?wsdl";
+	private static final String WS_92_FAILSTOP_NO_FT = "http://127.0.0.1:21092/choreography?wsdl";
+	private static final String WS_91_FAILSTOP_NO_FT = "http://127.0.0.1:21091/choreography?wsdl";
+	private static final String WS_90_FAILSTOP_NO_FT = "http://127.0.0.1:21090/choreography?wsdl";
 
-	private static final String WS_100_RELIABILITY_FT = "http://127.0.0.1:22100/choreography?wsdl";
-	private static final String WS_99_RELIABILITY_FT = "http://127.0.0.1:22099/choreography?wsdl";
-	private static final String WS_98_RELIABILITY_FT = "http://127.0.0.1:22098/choreography?wsdl";
-	private static final String WS_97_RELIABILITY_FT = "http://127.0.0.1:22097/choreography?wsdl";
-	private static final String WS_96_RELIABILITY_FT = "http://127.0.0.1:22096/choreography?wsdl";
-	private static final String WS_95_RELIABILITY_FT = "http://127.0.0.1:22095/choreography?wsdl";
-	private static final String WS_94_RELIABILITY_FT = "http://127.0.0.1:22094/choreography?wsdl";
-	private static final String WS_93_RELIABILITY_FT = "http://127.0.0.1:22093/choreography?wsdl";
-	private static final String WS_92_RELIABILITY_FT = "http://127.0.0.1:22092/choreography?wsdl";
-	private static final String WS_91_RELIABILITY_FT = "http://127.0.0.1:22091/choreography?wsdl";
-	private static final String WS_90_RELIABILITY_FT = "http://127.0.0.1:22090/choreography?wsdl";
+	private static final String WS_100_FAILSTOP_WITH_FT= "http://127.0.0.1:22100/choreography?wsdl";
+	private static final String WS_99_FAILSTOP_WITH_FT= "http://127.0.0.1:22099/choreography?wsdl";
+	private static final String WS_98_FAILSTOP_WITH_FT= "http://127.0.0.1:22098/choreography?wsdl";
+	private static final String WS_97_FAILSTOP_WITH_FT= "http://127.0.0.1:22097/choreography?wsdl";
+	private static final String WS_96_FAILSTOP_WITH_FT= "http://127.0.0.1:22096/choreography?wsdl";
+	private static final String WS_95_FAILSTOP_WITH_FT= "http://127.0.0.1:22095/choreography?wsdl";
+	private static final String WS_94_FAILSTOP_WITH_FT= "http://127.0.0.1:22094/choreography?wsdl";
+	private static final String WS_93_FAILSTOP_WITH_FT= "http://127.0.0.1:22093/choreography?wsdl";
+	private static final String WS_92_FAILSTOP_WITH_FT= "http://127.0.0.1:22092/choreography?wsdl";
+	private static final String WS_91_FAILSTOP_WITH_FT= "http://127.0.0.1:22091/choreography?wsdl";
+	private static final String WS_90_FAILSTOP_WITH_FT= "http://127.0.0.1:22090/choreography?wsdl";
+	
+	private static final String WS_100_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23100/choreography?wsdl";
+	private static final String WS_99_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23099/choreography?wsdl";
+	private static final String WS_98_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23098/choreography?wsdl";
+	private static final String WS_97_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23097/choreography?wsdl";
+	private static final String WS_96_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23096/choreography?wsdl";
+	private static final String WS_95_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23095/choreography?wsdl";
+	private static final String WS_94_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23094/choreography?wsdl";
+	private static final String WS_93_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23093/choreography?wsdl";
+	private static final String WS_92_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23092/choreography?wsdl";
+	private static final String WS_91_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23091/choreography?wsdl";
+	private static final String WS_90_FAULTY_RESPONSE_NO_FT= "http://127.0.0.1:23090/choreography?wsdl";
+
+	private static final String WS_100_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24100/choreography?wsdl";
+	private static final String WS_99_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24099/choreography?wsdl";
+	private static final String WS_98_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24098/choreography?wsdl";
+	private static final String WS_97_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24097/choreography?wsdl";
+	private static final String WS_96_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24096/choreography?wsdl";
+	private static final String WS_95_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24095/choreography?wsdl";
+	private static final String WS_94_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24094/choreography?wsdl";
+	private static final String WS_93_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24093/choreography?wsdl";
+	private static final String WS_92_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24092/choreography?wsdl";
+	private static final String WS_91_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24091/choreography?wsdl";
+	private static final String WS_90_FAULTY_RESPONSE_WITH_FT= "http://127.0.0.1:24090/choreography?wsdl";
+
 
 	private static Logger log;
 
@@ -81,36 +106,37 @@ public class ReliabilityEvaluation {
 
 		}
 
-	}
-
+	} //end private class
+	
 	@BeforeClass
 	public static void setUp() {
 		log = Logger.getLogger(ReliabilityEvaluation.class);
 		log.info("Initiating RELIABILITY Evaluation");
 	}
 
-	@Test
-	public void evaluateMultipleFaultPossibilitiesWithoutFT()
+	
+//	@Test
+	public void evaluateMultipleFailStopInvokationsWithoutFT()
 			throws TimeoutException {
 
 		HashMap<Integer, WsInvoker> invokers = new HashMap<Integer, WsInvoker>();
-//		ArrayList<String> services = new ArrayList<String>();
-
+		// ArrayList<String> services = new ArrayList<String>();
+		// 
 		// services.add("http://godzilla.ime.usp.br:22090/Linear?wsdl");
 		// String wsdl = "http://127.0.0.1:21090/choreography?wsdl";
 		// setUp(wsdl, wsdl, services);// */
 
-		invokers.put(100, new WsInvoker(WS_100_RELIABILITY_NOFT));
-		invokers.put(99, new WsInvoker(WS_99_RELIABILITY_NOFT));
-		invokers.put(98, new WsInvoker(WS_98_RELIABILITY_NOFT));
-		invokers.put(97, new WsInvoker(WS_97_RELIABILITY_NOFT));
-		invokers.put(96, new WsInvoker(WS_96_RELIABILITY_NOFT));
-		invokers.put(95, new WsInvoker(WS_95_RELIABILITY_NOFT));
-		invokers.put(94, new WsInvoker(WS_94_RELIABILITY_NOFT));
-		invokers.put(93, new WsInvoker(WS_93_RELIABILITY_NOFT));
-		invokers.put(92, new WsInvoker(WS_92_RELIABILITY_NOFT));
-		invokers.put(91, new WsInvoker(WS_91_RELIABILITY_NOFT));
-		invokers.put(90, new WsInvoker(WS_90_RELIABILITY_NOFT));
+		invokers.put(100, new WsInvoker(WS_100_FAILSTOP_NO_FT));
+		invokers.put(99, new WsInvoker(WS_99_FAILSTOP_NO_FT));
+		invokers.put(98, new WsInvoker(WS_98_FAILSTOP_NO_FT));
+		invokers.put(97, new WsInvoker(WS_97_FAILSTOP_NO_FT));
+		invokers.put(96, new WsInvoker(WS_96_FAILSTOP_NO_FT));
+		invokers.put(95, new WsInvoker(WS_95_FAILSTOP_NO_FT));
+		invokers.put(94, new WsInvoker(WS_94_FAILSTOP_NO_FT));
+		invokers.put(93, new WsInvoker(WS_93_FAILSTOP_NO_FT));
+		invokers.put(92, new WsInvoker(WS_92_FAILSTOP_NO_FT));
+		invokers.put(91, new WsInvoker(WS_91_FAILSTOP_NO_FT));
+		invokers.put(90, new WsInvoker(WS_90_FAILSTOP_NO_FT));
 
 		// warm up invokation
 		for (int key : invokers.keySet()) {
@@ -130,51 +156,13 @@ public class ReliabilityEvaluation {
 		}
 	}
 
-//	static void setUp(String wsdl, String endpoint, List<String> webServices) {
-//		ChoreographyEndpoint chor = new ChoreographyEndpoint();
-//
-//		chor.nextProxyUrl = wsdl;
-//		chor.wsMethodName = "addOne";
-//
-//		chor.myProxy = new Proxy();
-//
-//		for (String currentEndpoint : webServices) {
-//			System.out.println("adding " + currentEndpoint);
-//			chor.myProxy.addWebService(currentEndpoint);
-//
-//		}
-//
-//		System.out.println("Publishing Proxy at " + endpoint);
-//		Endpoint.publish(endpoint, chor);
-//
-//		System.out.println("Done! Ready to warm up!");
-//	}
-
-//	static void setUp(String wsdl, int retryAmount, String endpoint,
-//			String webService) {
-//		ChoreographyEndpoint chor = new ChoreographyEndpoint();
-//
-//		chor.nextProxyUrl = wsdl;
-//		chor.wsMethodName = "addOne";
-//
-//		chor.myProxy = new Proxy();
-//
-//		System.out.println("adding " + webService);
-//		chor.myProxy.addWebService(webService);
-//
-//		System.out.println("Publishing Proxy at " + endpoint);
-//		Endpoint.publish(endpoint, chor);
-//
-//		System.out.println("Done! Ready to warm up!");
-//	}
-
-	@Test
-	public void evaluateMultipleFaultPossibilitiesWithFT()
+//	@Test
+	public void evaluateMultipleFailStopWithFT()
 			throws TimeoutException {
 
 		HashMap<Integer, WsInvoker> invokers = new HashMap<Integer, WsInvoker>();
-		ArrayList<String> services = new ArrayList<String>();
-
+		// ArrayList<String> services = new ArrayList<String>();
+		//		
 		// services.add("http://godzilla.ime.usp.br:22090/Linear?wsdl");
 		// services.add("http://godzilla.ime.usp.br:23090/Linear?wsdl");
 		// services.add("http://godzilla.ime.usp.br:24090/Linear?wsdl");
@@ -182,17 +170,17 @@ public class ReliabilityEvaluation {
 		// setUp(wsdl, wsdl, services);// */
 		// invokers.put(90, new WsInvoker(wsdl));
 
-		 invokers.put(100, new WsInvoker(WS_100_RELIABILITY_FT));
-		 invokers.put(99, new WsInvoker(WS_99_RELIABILITY_FT));
-		 invokers.put(98, new WsInvoker(WS_98_RELIABILITY_FT));
-		 invokers.put(97, new WsInvoker(WS_97_RELIABILITY_FT));
-		 invokers.put(96, new WsInvoker(WS_96_RELIABILITY_FT));
-		 invokers.put(95, new WsInvoker(WS_95_RELIABILITY_FT));
-		 invokers.put(94, new WsInvoker(WS_94_RELIABILITY_FT));
-		 invokers.put(93, new WsInvoker(WS_93_RELIABILITY_FT));
-		 invokers.put(92, new WsInvoker(WS_92_RELIABILITY_FT));
-		 invokers.put(91, new WsInvoker(WS_91_RELIABILITY_FT));
-		 invokers.put(90, new WsInvoker(WS_90_RELIABILITY_FT));
+		 invokers.put(100, new WsInvoker(WS_100_FAILSTOP_WITH_FT));
+		 invokers.put(99, new WsInvoker(WS_99_FAILSTOP_WITH_FT));
+		 invokers.put(98, new WsInvoker(WS_98_FAILSTOP_WITH_FT));
+		 invokers.put(97, new WsInvoker(WS_97_FAILSTOP_WITH_FT));
+		 invokers.put(96, new WsInvoker(WS_96_FAILSTOP_WITH_FT));
+		 invokers.put(95, new WsInvoker(WS_95_FAILSTOP_WITH_FT));
+		 invokers.put(94, new WsInvoker(WS_94_FAILSTOP_WITH_FT));
+		 invokers.put(93, new WsInvoker(WS_93_FAILSTOP_WITH_FT));
+		 invokers.put(92, new WsInvoker(WS_92_FAILSTOP_WITH_FT));
+		 invokers.put(91, new WsInvoker(WS_91_FAILSTOP_WITH_FT));
+		 invokers.put(90, new WsInvoker(WS_90_FAILSTOP_WITH_FT));
 
 		// warm up invokation
 		for (int key : invokers.keySet()) {
@@ -201,13 +189,93 @@ public class ReliabilityEvaluation {
 
 		log.info("--------------------------------------------");
 		log.info("--------------------------------------------");
-		log.info("  Starting reliability evaluation. WITH FT.");
+		log.info("  Starting FAIL STOP reliability evaluation. WITH FT.");
 		log.info("--------------------------------------------");
 		log.info("--------------------------------------------");
 		for (int key : invokers.keySet()) {
 			log.info("Reliability set at " + key + '\n'
 					+ "Starting evaluation with 1000 parallel requests");
 			int invokeAmount = 1000;
+			multipleInvokations(invokers.get(key), invokeAmount);
+		}
+	}
+
+	@Test
+	public void evaluateMultipleFaultyResponseInvokationsWithoutFT()
+			throws TimeoutException {
+
+		HashMap<Integer, WsInvoker> invokers = new HashMap<Integer, WsInvoker>();
+
+		invokers.put(100, new WsInvoker(WS_100_FAULTY_RESPONSE_NO_FT));
+		invokers.put(99, new WsInvoker(WS_99_FAULTY_RESPONSE_NO_FT));
+		invokers.put(98, new WsInvoker(WS_98_FAULTY_RESPONSE_NO_FT));
+		invokers.put(97, new WsInvoker(WS_97_FAULTY_RESPONSE_NO_FT));
+		invokers.put(96, new WsInvoker(WS_96_FAULTY_RESPONSE_NO_FT));
+		invokers.put(95, new WsInvoker(WS_95_FAULTY_RESPONSE_NO_FT));
+		invokers.put(94, new WsInvoker(WS_94_FAULTY_RESPONSE_NO_FT));
+		invokers.put(93, new WsInvoker(WS_93_FAULTY_RESPONSE_NO_FT));
+		invokers.put(92, new WsInvoker(WS_92_FAULTY_RESPONSE_NO_FT));
+		invokers.put(91, new WsInvoker(WS_91_FAULTY_RESPONSE_NO_FT));
+		invokers.put(90, new WsInvoker(WS_90_FAULTY_RESPONSE_NO_FT));
+
+		// warm up invokation
+		for (int key : invokers.keySet()) {
+			warmUpInvokation(invokers.get(key));
+		}
+
+		log.info("--------------------------------------------");
+		log.info("--------------------------------------------");
+		log.info("Starting reliability evaluation. WITHOUT FT.");
+		log.info("--------------------------------------------");
+		log.info("--------------------------------------------");
+		for (int key : invokers.keySet()) {
+			log.info("Reliability set at " + key + '\n'
+					+ "Starting evaluation with 1000 parallel requests");
+			int invokeAmount = 1000;
+			multipleInvokations(invokers.get(key), invokeAmount);
+		}
+	}
+
+//	@Test
+	public void evaluateMultipleFaultyResponseWithFT()
+			throws TimeoutException {
+
+		HashMap<Integer, WsInvoker> invokers = new HashMap<Integer, WsInvoker>();
+		// ArrayList<String> services = new ArrayList<String>();
+		//		
+		// services.add("http://godzilla.ime.usp.br:22090/Linear?wsdl");
+		// services.add("http://godzilla.ime.usp.br:23090/Linear?wsdl");
+		// services.add("http://godzilla.ime.usp.br:24090/Linear?wsdl");
+		// String wsdl = "http://127.0.0.1:20090/choreography?wsdl";
+		// setUp(wsdl, wsdl, services);// */
+		// invokers.put(90, new WsInvoker(wsdl));
+
+		 invokers.put(100, new WsInvoker(WS_100_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(99, new WsInvoker(WS_99_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(98, new WsInvoker(WS_98_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(97, new WsInvoker(WS_97_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(96, new WsInvoker(WS_96_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(95, new WsInvoker(WS_95_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(94, new WsInvoker(WS_94_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(93, new WsInvoker(WS_93_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(92, new WsInvoker(WS_92_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(91, new WsInvoker(WS_91_FAULTY_RESPONSE_WITH_FT));
+		 invokers.put(90, new WsInvoker(WS_90_FAULTY_RESPONSE_WITH_FT));
+
+		// warm up invokation
+		for (int key : invokers.keySet()) {
+			warmUpInvokation(invokers.get(key));
+		}
+
+		log.info("--------------------------------------------");
+		log.info("--------------------------------------------");
+		log.info("  Starting FAIL STOP reliability evaluation. WITH FT.");
+		log.info("--------------------------------------------");
+		log.info("--------------------------------------------");
+		for (int key : invokers.keySet()) {
+			log.info("Reliability set at " + key + '\n'
+					+ "Starting evaluation with 1000 parallel requests");
+			int invokeAmount = 2;
 			multipleInvokations(invokers.get(key), invokeAmount);
 		}
 	}
