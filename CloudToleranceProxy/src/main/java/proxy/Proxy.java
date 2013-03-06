a98ebc47a3620bd84ded4c87156d1424678471b8
Felipe Guimarães <felipepg@ime.usp.br>
2013-03-06 17:40:01 -0300

    Added possibility of setting the Proxy to be aware of faulty response components

--- CloudToleranceProxy/src/main/java/proxy/Proxy.java
+++ CloudToleranceProxy/src/main/java/proxy/Proxy.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 import proxy.techniques.Active;
 import proxy.techniques.FaultToleranceTechnique;
 import proxy.techniques.Retry;
+import proxy.techniques.Voting;
 import proxy.webservice.handlers.WsInvoker;
 
 public class Proxy {
@@ -16,7 +17,7 @@ public class Proxy {
 	private HashMap<String, FaultToleranceTechnique> availableTechniques;
 	private FaultToleranceTechnique currentTechnique;
 	private List<WsInvoker> invokerList;
-	private Logger log = Logger.getLogger(Proxy.class); 
+	private Logger log = Logger.getLogger(Proxy.class);
 	
 	public Proxy() {
 		availableTechniques = new HashMap<String, FaultToleranceTechnique>();
@@ -25,6 +26,7 @@ public class Proxy {
 
 		availableTechniques.put("Retry", new Retry());
 		availableTechniques.put("Active", new Active());
+		availableTechniques.put("Voting", new Voting());
 		
 		if(invokerList.size() <= 1){
 			log.info("Retry Technique");
@@ -37,6 +39,12 @@ public class Proxy {
 		currentTechnique.addAvailableInvokers(invokerList);
 	}
 	
+	public void setUnreliableServices(){
+		if (availableTechniques.containsKey("Voting")){
+			currentTechnique = availableTechniques.get("Voting");
+					
+		}
+	}
 	public void addWebService(String wsdlEndpoint) {
 		WsInvoker newService = new WsInvoker(wsdlEndpoint);
 		invokerList.add(newService);
