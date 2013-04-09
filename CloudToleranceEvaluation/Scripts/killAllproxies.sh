72163afaa2f5a3a9eb8fee54acaa8c774116bd0e
Felipe Guimarães <felipepg@ime.usp.br>
2013-04-09 15:59:48 -0300

    Removed echo feature to enable compatibility

--- CloudToleranceEvaluation/Scripts/killAllproxies.sh
+++ CloudToleranceEvaluation/Scripts/killAllproxies.sh
@@ -6,31 +6,31 @@ source locations.sh
 source locations.sh
 
 echo "Killing proxies at $server1..."
-ssh $user@$server1 pkill -ef Endpoint 
+ssh $user@$server1 pkill -f Endpoint 
 
 echo "Killing proxies at $server2..."
-ssh $user@$server2 pkill -ef Endpoint 
+ssh $user@$server2 pkill -f Endpoint 
 
 echo "Killing proxies at $server3..."
-ssh $user@$server3 pkill -ef Endpoint 
+ssh $user@$server3 pkill -f Endpoint 
 
 echo "Killing proxies at $server4..."
-ssh $user@$server4 pkill -ef Endpoint 
+ssh $user@$server4 pkill -f Endpoint 
 
 echo "Killing proxies at $server5..."
-ssh $user@$server5 pkill -ef Endpoint 
+ssh $user@$server5 pkill -f Endpoint 
 
 echo "Killing proxies at $server6..."
-ssh $user@$server6 pkill -ef Endpoint 
+ssh $user@$server6 pkill -f Endpoint 
 
 echo "Killing proxies at $server7..."
-ssh $user@$server7 pkill -ef Endpoint 
+ssh $user@$server7 pkill -f Endpoint 
 
 echo "Killing proxies at $server8..."
-ssh $user@$server8 pkill -ef Endpoint 
+ssh $user@$server8 pkill -f Endpoint 
 
 echo "Killing proxies at $server9..."
-ssh $user@$server9 pkill -ef Endpoint 
+ssh $user@$server9 pkill -f Endpoint 
 
 echo "Killing proxies at $server0..."
-ssh $user@$server0 pkill -ef Endpoint 
+ssh $user@$server0 pkill -f Endpoint 
