bb8209089245bb883190e58a1505264a8241e680
Felipe Guimarães <felipepg@ime.usp.br>
2013-04-10 14:59:16 -0300

    listAllProxies.sh no longer lists ssh processes

--- CloudToleranceEvaluation/Scripts/listAllProxies.sh
+++ CloudToleranceEvaluation/Scripts/listAllProxies.sh
@@ -3,22 +3,22 @@
 source locations.sh
 
 echo "Listing proxies at $server1..."
-ssh $user@$server1 ps aux | grep Endpoint 
+ssh $user@$server1 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server2..."
-ssh $user@$server2 ps aux | grep Endpoint 
+ssh $user@$server2 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server3..."
-ssh $user@$server3 ps aux | grep Endpoint 
+ssh $user@$server3 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server4..."
-ssh $user@$server4 ps aux | grep Endpoint 
+ssh $user@$server4 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server5..."
-ssh $user@$server5 ps aux | grep Endpoint 
+ssh $user@$server5 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server6..."
-ssh $user@$server6 ps aux | grep Endpoint 
+ssh $user@$server6 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server7..."
-ssh $user@$server7 ps aux | grep Endpoint 
+ssh $user@$server7 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server8..."
-ssh $user@$server8 ps aux | grep Endpoint 
+ssh $user@$server8 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server9..."
-ssh $user@$server9 ps aux | grep Endpoint 
+ssh $user@$server9 ps aux | grep Endpoint | grep -v ssh
 echo "Listing proxies at $server0..."
-ssh $user@$server0 ps aux | grep Endpoint 
+ssh $user@$server0 ps aux | grep Endpoint | grep -v ssh
