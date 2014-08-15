318874e65782c23a3101d6974c76d441cddb8998
Felipe Pontes <fpontes100@gmail.com>
2014-08-15 15:40:26 -0300

    BPMN test class

--- CloudToleranceProxy/src/test/java/proxy/utils/BPMNTaskTest.java
+++ CloudToleranceProxy/src/test/java/proxy/utils/BPMNTaskTest.java
@@ -10,13 +10,15 @@ public class BPMNTaskTest {
 
 	@Test
 	public void test() {
-		BPMNTask task1 = new BPMNTask("http://0.0.0.0:2430/choreography",
+		BPMNTask task1 = new BPMNTask("name", "http://0.0.0.0:2430/choreography",
 				"http://127.0.0.1:2431/proxy?wsdl", "addOne",
 				"http://127.0.0.1:2401/Linear?wsdl" );
 		assertEquals("http://0.0.0.0:2430/choreography", task1.getEndpoint());
 		assertEquals("http://127.0.0.1:2431/proxy?wsdl", task1.getNextLink());
 		assertEquals("http://127.0.0.1:2401/Linear?wsdl", task1.getProvidingServicesWsdlList().get(0));
 		assertEquals("addOne", task1.getMethodName());
+		assertEquals("name", task1.getName());
+		
 		
 	}
 
